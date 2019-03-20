4812edd22369a69ac1f559d2535ef37c6ec5808d
Henry Blevins <heblevi@gmail.com>
2019-03-20 12:22:02 -0400

    Test components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,23 @@
 import React from 'react';
+import { TodoList } from './components/TodoComponents';
+
+const testTodos = [
+    {
+        task: 'Organize Garage',
+        id: 1528817077286,
+        completed: false
+    },
+    {
+        task: 'Bake Cookies',
+        id: 1528817084358,
+        completed: false
+    },
+    {
+        task: 'Make Breakfast',
+        id: 1528817084359,
+        completed: true
+    }
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
@@ -8,6 +27,7 @@ class App extends React.Component {
     return (
       <div>
         <h2>Todo List</h2>
+        <TodoList todos={testTodos} />
       </div>
     );
   }
---
+++ src/components/TodoComponents/index.js
@@ -0,0 +1,4 @@
+import Todo from './Todo.js';
+import TodoList from './TodoList.js';
+
+export { Todo, TodoList };
