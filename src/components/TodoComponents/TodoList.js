4113b1b3458e41c0909c45bfb217e5d163fe7975
Henry Blevins <heblevi@gmail.com>
2019-03-20 17:41:24 -0400

    Gracefully handle no todos or all filtered todos

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -3,12 +3,15 @@ import './Todo.css';
 import Todo from './Todo.js';
 
 const TodoList = ({todos, toggleCompleted, filter}) => {
+    const filteredTodos = todos.filter(todo => todo.task.toLowerCase().includes(filter.trim().toLowerCase()));
     return <div className="todo-list">
-             {todos
-              .filter(todo => todo.task.toLowerCase().includes(filter.trim().toLowerCase()))
-              .map(todo => <Todo key={todo.id}
-                                 toggleCompleted={toggleCompleted}
-                                 {...todo} />)}
+             {(filteredTodos && filteredTodos.length > 0)
+               ? filteredTodos.map(todo => <Todo key={todo.id}
+                                                 toggleCompleted={toggleCompleted}
+                                                 {...todo} />)
+               : <div className="todo-empty">
+                   {filter.trim() !== "" ? "Try expanding your filter" : "Add a new todo" }
+                 </div>}
            </div>;
 };
 
