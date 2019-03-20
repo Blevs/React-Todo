638a7352cd7079af9444c3cc491eade0964f584a
Henry Blevins <heblevi@gmail.com>
2019-03-20 12:17:20 -0400

    Add TodoList component

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,11 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from 'react';
+import './Todo.css';
+import Todo from './Todo.js';
+
+const TodoList = ({todos}) => {
+    return <div className="todo-list">
+             {todos.map(todo => <Todo {...todo} />)}
+           </div>;
+};
+
+export default TodoList;
